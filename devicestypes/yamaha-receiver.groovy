1cb5f85a56b70deecbbb221a12a5754490a46aec
codahq <codahq@users.noreply.github.com>
2019-03-13 03:57:15 -0600

    + parameterized logging

--- devicestypes/yamaha-receiver.groovy
+++ devicestypes/yamaha-receiver.groovy
@@ -28,8 +28,23 @@ metadata {
       //input name: "receiverName", type: "text", title: "Name", required: true, defaultValue: "Yamaha"
       input name: "receiverIp", type: "text", title: "IP", required: true
       input name: "receiverZones", type: "enum", title: "Zones", required: true, multiple: true, options: ["Main_Zone", "Zone_B", "Zone_2", "Zone_3", "Zone_4"]
+      input name: "descriptionTextEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: false
+      input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+      input name: "traceLogEnable", type: "bool", title: "Enable trace logging", defaultValue: false
     }
   }
+}
+
+private logInfo(msg) {
+  if (descriptionTextEnable) log.info msg
+}
+
+def logDebug(msg) {
+  if (logEnable) log.debug msg
+}
+
+def logTrace(msg) {
+  if (traceLogEnable) log.trace msg
 }
 
 def updated() {
@@ -46,16 +61,16 @@ def parse(String description) {
   def map = parseLanMessage(description)
 
   def body = getHttpBody(map.body);
-  //log.trace "Headers: ${map.headers}"
-  //log.trace "Body: ${body}"
+  logTrace "Headers: ${map.headers}"
+  logTrace "Body: ${body}"
 
   updateZoneDevices(body.children()[0])
 }
 
 private updateZoneDevices(evt) {
-  //log.debug "updateZoneDevices: ${evt.toString()}"
+  logDebug "updateZoneDevices: ${evt.toString()}"
   if (evt.name() == "System") {
-    //log.debug "Update all zones"
+    logDebug "Update all zones"
     childDevices *.zone(evt)
     return
   }
@@ -74,7 +89,6 @@ private updateZoneDevices(evt) {
 
 private addChildDevices() {
   // add yamaha zone device(s)
-  //settings.receiverZones.properties.each { log.info "derp ${it}" }
 
   //temporary workaround to add Strings to lists
   def selectedZones = []
@@ -88,7 +102,7 @@ private addChildDevices() {
     def deviceId = getDeviceId(it)
     if (!getChildDevice(deviceId)) {
       addChildDevice("codahq-hubitat", "Yamaha Zone", deviceId, [name: "Yamaha Zone ${it}", label: "${device.name} - Zone ${it}", isComponent: false])
-      log.debug "Added Yamaha zone: ${deviceId}"
+      logInfo "Added Yamaha zone: ${deviceId}"
     }
   }
 
@@ -97,13 +111,13 @@ private addChildDevices() {
 
 private removeChildDevices() {
   getChildDevices().each {
-    log.debug "deleting ${it}"
+    log.warn "deleting ${it}"
     deleteChildDevice(it.deviceNetworkId)
   }
 }
 
 private sendCommand(body) {
-  log.debug "Yamaha Network Receiver send command: ${groovy.xml.XmlUtil.escapeXml(body)}"
+  logDebug "Yamaha Network Receiver send command: ${groovy.xml.XmlUtil.escapeXml(body)}"
 
   def hubAction = new hubitat.device.HubAction(
     headers: [HOST: getReceiverAddress()],
@@ -116,9 +130,8 @@ private sendCommand(body) {
 
 private getHttpHeaders(headers) {
   def obj = [: ]
-  new String(headers.decodeBase64()).split("\r\n").each {
-  param ->
-    def nameAndValue = param.split(":")
+  new String(headers.decodeBase64()).split("\r\n").each { param ->
+      def nameAndValue = param.split(":")
     obj[nameAndValue[0]] = (nameAndValue.length == 1) ? "" : nameAndValue[1].trim()
   }
   return obj
@@ -177,7 +190,8 @@ private updateDNI() {
 private getHostAddress() {
   if (getDeviceDataByName("ip") && getDeviceDataByName("port")) {
     return "${getDeviceDataByName("ip")}:${getDeviceDataByName("port")}"
-  } else {
+  }
+  else {
     return "${ip}:80"
   }
 }
